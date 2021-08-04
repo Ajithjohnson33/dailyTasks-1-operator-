d50395c545568cbab1bf2d08f5a0a0a4691491a2
Ajith Johnson33 <ajithjohnson33@gmail.com>
2021-08-04 15:01:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+       document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function add()
+{
+    num1 = parseInt (document.getElementById("firstNumber").value);
+    num2 = parseInt (document.getElementById("secondNumber").value);
+
+    parseInt(document.getElementById ("result") .innerHTML = num1 + num2);
+
+}
+
+function substract()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 - num2;
+
+}
+
+function add()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 % num2;
+
+}
+
+function modulus()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 % num2;
+
+}
+
+function greater()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 > num2;
+
+}
+
+function lesser()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 < num2;
+
+}
+
+function equal()
+{
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+
+   document.getElementById ("result") .innerHTML = num1 == num2;
+
+}
+
+
+
+
+
+
